9092cf2c6014f4598525a4f2d255193f5f6c3147
Mariia Kasianova <kasyanovamg@gmail.com>
2018-10-17 01:32:11 +0400

    prototype of shulte working test

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Link to="/shulte" className="">Shulte</Link>
+        </header>
+        
+      </div>
+    );
+  }
+}
+
+export default Home;
