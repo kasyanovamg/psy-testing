617a98275e0f24ea348aa0d857b276ccbed47283
kasyanovamg <kasyanovamg@gmail.com>
2018-11-06 16:43:38 +0400

    Updates

---
+++ precache-manifest.5c1e4ad16c0e0b76e775d5d52d0810f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b20618a6714cd820b3",
+    "url": "/psy-testing/static/js/runtime~main.b6b20618.js"
+  },
+  {
+    "revision": "a7027bf612642adfdd04",
+    "url": "/psy-testing/static/js/main.a7027bf6.chunk.js"
+  },
+  {
+    "revision": "9f3a2da1b0d50a7974ca",
+    "url": "/psy-testing/static/js/1.9f3a2da1.chunk.js"
+  },
+  {
+    "revision": "a7027bf612642adfdd04",
+    "url": "/psy-testing/static/css/main.194c5647.chunk.css"
+  },
+  {
+    "revision": "5cafcc9cd746c3639087d3fc96dd4c93",
+    "url": "/psy-testing/index.html"
+  }
+];
