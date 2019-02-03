65f18c445cd4a74b82dcd9fb7a58f957fdbd58d8
Mariia Kasianova <kasyanovamg@gmail.com>
2019-02-03 23:43:57 +0400

    added sign in sign out options

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Signin from './components/auth/SignIn';
 import Signup from './components/auth/SignUp';
 class App extends Component {
   render() {
     return (
-      <div>
-        <header>
-          Header
-          <Link to='/signin'>Signin</Link>
-          <Link to='/signup'>Signup</Link>
-        </header>
-        <Switch>
-          <Route exact path='/signin' component={Signin} />
-          <Route exact path='/signup' component={Signup} />
-          <Redirect to='/' />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path='/' component={Navbar} />
+        <Route exact path='/signin' component={Signin} />
+        <Route exact path='/signup' component={Signup} />
+        <Redirect to='/' />
+      </Switch>
     );
   }
 }
