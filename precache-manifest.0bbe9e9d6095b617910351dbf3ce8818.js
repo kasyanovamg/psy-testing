460427dcbad4b136c00586944a796c82cc387088
Mariia Kasianova <kasyanovamg@gmail.com>
2019-10-15 01:27:34 +0400

    Updates

---
+++ precache-manifest.0bbe9e9d6095b617910351dbf3ce8818.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b20618a6714cd820b3",
+    "url": "/psy-testing/static/js/runtime~main.b6b20618.js"
+  },
+  {
+    "revision": "d6583171859841e50ba1",
+    "url": "/psy-testing/static/js/main.d6583171.chunk.js"
+  },
+  {
+    "revision": "b69ab95704af4412e9f2",
+    "url": "/psy-testing/static/js/1.b69ab957.chunk.js"
+  },
+  {
+    "revision": "d6583171859841e50ba1",
+    "url": "/psy-testing/static/css/main.fa61df66.chunk.css"
+  },
+  {
+    "revision": "b40c31677eb20897a19e979defe46896",
+    "url": "/psy-testing/index.html"
+  }
+];
