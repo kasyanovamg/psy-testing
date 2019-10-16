38f901e43e98789f23f1f8025bde6b078a7c0275
Mariia Kasianova <kasyanovamg@gmail.com>
2019-10-17 00:40:16 +0400

    Updates

--- precache-manifest.0fec9e80148567586450897e21f88177.js
+++ precache-manifest.0fec9e80148567586450897e21f88177.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/psy-testing/static/js/runtime~main.b6b20618.js"
   },
   {
-    "revision": "ff80ee69acacdb846013",
-    "url": "/psy-testing/static/js/main.ff80ee69.chunk.js"
+    "revision": "3ecf8463a2d072339c11",
+    "url": "/psy-testing/static/js/main.3ecf8463.chunk.js"
   },
   {
     "revision": "b69ab95704af4412e9f2",
     "url": "/psy-testing/static/js/1.b69ab957.chunk.js"
   },
   {
-    "revision": "ff80ee69acacdb846013",
+    "revision": "3ecf8463a2d072339c11",
     "url": "/psy-testing/static/css/main.83cfc846.chunk.css"
   },
   {
-    "revision": "8ddac6af6df3ad434766dd8589f5ef9d",
+    "revision": "6f4d31d51edc0eafe7d50499f5a137ad",
     "url": "/psy-testing/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/psy-testing/precache-manifest.1da626eafc557ee6de53f481aa727fff.js"
+  "/psy-testing/precache-manifest.0fec9e80148567586450897e21f88177.js"
 );
 
 workbox.clientsClaim();
