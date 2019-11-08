8f158c3a2f9fc37df9bb40612e135c9fe1dc101c
Mariia Kasianova <kasyanovamg@gmail.com>
2019-11-09 01:31:18 +0400

    adding summary results

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -14,6 +14,47 @@ const getProjects = createSelector(
 );
 
 const Summary = ({ projects, auth }) => {
+  if (auth.uid === 'UuE6qoYQJAV63WZZHvvbJnTLiKE2') {
+    //console.log(projects)
+    const shulteResults = projects.filter(project => project.generalResult.shulte && (project.generalResult.shulte !== 0));
+    //console.log(shulteResults);
+    const dateArr = shulteResults.map(project => project.createdAt ? project.createdAt.toDate().toLocaleDateString() : 0);
+
+    // let shul = [];
+    // for (let i=0; i < projects.length; i++) {
+    //   if (projects[i].generalResult.shulte && (projects[i].generalResult.shulte !== 0)) {
+    //     shul.push(projects[i].generalResult.shulte)
+    //   }
+    // }
+    // console.log(shul)
+    let counter = 1;
+    let temp = {
+      [counter]: []
+    };
+
+    for (let i=0; i < projects.length; i++) {
+     // console.log(projects[i])
+      //console.log(temp[counter].map(item =>  [item.authorId, projects[i].authorId]));
+     // let check = temp[counter].filter(item => item.authorId !== projects[i].authorId);
+     // console.log(check)
+      if (temp[counter].filter(item => item.authorId !== projects[i].authorId) !== []) {
+        //console.log(temp[counter].filter(item => item.authorId !== projects[i].authorId))
+        temp[counter].push(projects[i]);
+        console.log("we pushed")
+      } else {
+        console.log('else')
+        temp[counter].push(projects[i]);
+        counter++;
+      }
+    }
+console.log(temp)
+    return <div>
+      <Charts date={dateArr}
+              results={shulteResults.map(project => project.generalResult ? project.generalResult.shulte || 0 : 0)}
+              name='Таблицы Шульте'
+      />
+    </div>
+  }
   const filteredProjects = projects.filter(project => project.authorId === auth.uid);
   const dateArray = filteredProjects.map(project => project.createdAt ? project.createdAt.toDate().toLocaleDateString() : 0);
   return (
