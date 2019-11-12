d701153a064d09a257e3604176679f306978a02c
Mariia Kasianova <kasyanovamg@gmail.com>
2019-11-13 01:04:56 +0400

    adding summary results for shulte

--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -11,51 +11,77 @@ const getAllProjects = (state) => get(state, 'firestore.ordered.projects', []);
 const getProjects = createSelector(
   getAllProjects,
   (projects) => orderBy(projects, ['createdAt'],['asc'])
+    .filter(project => project.authorId !== 'UuE6qoYQJAV63WZZHvvbJnTLiKE2'),
 );
+
+const getAverage = (projects) => {
+
+  let counter = 1;
+  let temp = {
+    [counter]: []
+  };
+
+  for (let i = 0; i < projects.length; i++) {
+    let check = temp[counter].find(item => item.authorId === projects[i].authorId);
+    if (!check) {
+      temp[counter].push(projects[i]);
+    } else {
+      counter = counter + 1;
+      temp[counter] = [];
+      temp[counter].push(projects[i]);
+    }
+  }
+  const results = {};
+  Object.values(temp).forEach(
+    (project, i) => {
+      results[i] = {
+        shulte: [],
+        shulteRed: [],
+        count: []
+      };
+      project.forEach(p => {
+        if (p.generalResult) {
+          if (p.generalResult.shulte > 0) {
+            results[i].shulte.push(p.generalResult.shulte)
+          }
+        }
+
+      })
+    });
+
+  return results;
+
+};
 
 const Summary = ({ projects, auth }) => {
   if (auth.uid === 'UuE6qoYQJAV63WZZHvvbJnTLiKE2') {
     //console.log(projects)
     const shulteResults = projects.filter(project => project.generalResult.shulte && (project.generalResult.shulte !== 0));
-    //console.log(shulteResults);
+    const perceptionResults = projects.filter(project => project.generalResult.perception && (project.generalResult.perception !== 0));
+    const memoryResults = projects.filter(project => project.generalResult.memoryWords && (project.generalResult.memoryWords !== 0));
+
     const dateArr = shulteResults.map(project => project.createdAt ? project.createdAt.toDate().toLocaleDateString() : 0);
 
-    // let shul = [];
-    // for (let i=0; i < projects.length; i++) {
-    //   if (projects[i].generalResult.shulte && (projects[i].generalResult.shulte !== 0)) {
-    //     shul.push(projects[i].generalResult.shulte)
-    //   }
-    // }
-    // console.log(shul)
-    let counter = 1;
-    let temp = {
-      [counter]: []
-    };
+    const average = getAverage(projects);
+    console.log(average);
 
-    for (let i=0; i < projects.length; i++) {
-     // console.log(projects[i])
-      //console.log(temp[counter].map(item =>  [item.authorId, projects[i].authorId]));
-     // let check = temp[counter].filter(item => item.authorId !== projects[i].authorId);
-     // console.log(check)
-      if (temp[counter].filter(item => item.authorId !== projects[i].authorId) !== []) {
-        //console.log(temp[counter].filter(item => item.authorId !== projects[i].authorId))
-        temp[counter].push(projects[i]);
-        console.log("we pushed")
-      } else {
-        console.log('else')
-        temp[counter].push(projects[i]);
-        counter++;
-      }
-    }
-console.log(temp)
     return <div>
       <Charts date={dateArr}
               results={shulteResults.map(project => project.generalResult ? project.generalResult.shulte || 0 : 0)}
               name='Таблицы Шульте'
       />
+      <Charts date={dateArr}
+              results={perceptionResults.map(project => project.generalResult ? project.generalResult.perception || 0 : 0)}
+              name='Корректурные пробы'
+      />
+      <Charts date={dateArr}
+              results={memoryResults.map(project => project.generalResult ? project.generalResult.memoryWords || 0 : 0)}
+              name='Запоминание слов'
+      />
     </div>
   }
   const filteredProjects = projects.filter(project => project.authorId === auth.uid);
+  console.log(filteredProjects)
   const dateArray = filteredProjects.map(project => project.createdAt ? project.createdAt.toDate().toLocaleDateString() : 0);
   return (
     <div className="card z-depth-0 project-summary summary-container">
@@ -98,6 +124,6 @@ const mapStateToProps = (state) => {
 export default compose(
   connect(mapStateToProps),
   firestoreConnect([
-    { collection: 'projects' }
+    { collection: 'projects' },
   ])
 )(Summary)
