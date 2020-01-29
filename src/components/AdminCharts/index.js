76598aaf0bb6298a5ba7241a6082d7ce3c041212
Mariia Kasianova <kasyanovamg@gmail.com>
2020-01-29 19:41:44 +0400

    fixed the graphics

--- src/components/AdminCharts/index.js
+++ src/components/AdminCharts/index.js
@@ -11,6 +11,11 @@ function getNormalizedContent(contentSettingsMapping) {
   return get(normalizedData, 'entities.elements', {});
 }
 
+function random_rgba() {
+  var o = Math.round, r = Math.random, s = 255;
+  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
 export function AdminCharts({date, results, name = ''}) {
 //normalize to make id the key and array with {result, attempt} or array of result for each user
   // const res = results.map()
@@ -20,54 +25,33 @@ export function AdminCharts({date, results, name = ''}) {
   });
 
   console.log('newObj', newObj);
-  const r = results.map(re => re / 2);
+  const dataset = Object.keys(newObj).map(obj => {
+    const color = random_rgba();
+    return {
+    label: obj,
+    fill: false,
+    lineTension: 0.1,
+    backgroundColor: 'rgba(75,192,192,0.4)',
+    borderColor: color,
+    borderCapStyle: 'butt',
+    borderDash: [],
+    borderDashOffset: 0.0,
+    borderJoinStyle: 'miter',
+    pointBorderColor: 'rgba(75,192,192,1)',
+    pointBackgroundColor: '#fff',
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointHoverBackgroundColor: color,
+    pointHoverBorderColor: 'rgba(220,220,220,1)',
+    pointHoverBorderWidth: 2,
+    pointRadius: 1,
+    pointHitRadius: 10,
+    data: newObj[obj]
+  }});
 
   const data = {
     labels: date,
-    datasets: [
-      {
-        label: name,
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(220,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: results
-      },
-      {
-        label: name,
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
-        pointBorderColor: 'rgba(75,192,192,1)',
-        pointBackgroundColor: '#fff',
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-        pointHoverBorderColor: 'rgba(0,220,220,1)',
-        pointHoverBorderWidth: 2,
-        pointRadius: 1,
-        pointHitRadius: 10,
-        data: r
-      },
-    ]
+    datasets: dataset,
   };
 
   return (
