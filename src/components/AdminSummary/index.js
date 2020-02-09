320f59373c21dca233eaec04b5aade6f02d9e60d
Mariia Kasianova <kasyanovamg@gmail.com>
2020-02-09 23:05:36 +0400

    changing data depending on a group

--- src/components/AdminSummary/index.js
+++ src/components/AdminSummary/index.js
@@ -7,18 +7,12 @@ import { compose } from 'redux';
 import { connect } from 'react-redux';
 import orderBy from 'lodash-es/orderBy';
 import { AdminCharts } from "../AdminCharts";
+import {Redirect} from "react-router-dom";
 
 const getAllProjects = (state) => get(state, 'firestore.ordered.projects', []);
 const getProjects = createSelector(
   getAllProjects,
   (projects) => orderBy(projects, ['createdAt'],['asc'])
-);
-const getAuthors = createSelector(
-  getAllProjects,
-  (projects) => {
-    const authors = new Set(projects.map(p => p.authorId));
-    return [...authors].concat('all');
-  }
 );
 const getGroups = createSelector(
   getAllProjects,
@@ -27,57 +21,76 @@ const getGroups = createSelector(
     return [...group].concat('all')
   }
 );
-
-const SummaryAdminView = ({ projects, auth, authors, groups }) => {
+const getFilteredAuthors = (projects) => {
+  const authors = new Set(projects.map(p => p.authorId));
+  return [...authors].concat('all');
+}
+const SummaryAdminView = ({projects, auth, groups}) => {
   const [selectedAuth, setAuth] = React.useState('all');
-  const onChangeAuth = React.useCallback((e) => setAuth(e.target.value), []);
+  const onChangeAuth = React.useCallback((e) => setAuth(e.target.value), [selectedAuth]);
   const [selectedGroup, setGroup] = React.useState('all');
-  const onChangeGroup = React.useCallback((e) => setGroup(e.target.value), []);
-  const filteredProjects = projects.filter(project => (project.authorId === selectedAuth || selectedAuth === 'all') &&
-  (project.group === selectedGroup || selectedGroup === 'all')
-  );
-  //const dateArray = filteredProjects.map(project => project.createdAt ? project.createdAt.toDate().toLocaleDateString() : 0);
+  const onChangeGroup = React.useCallback((e) => setGroup(e.target.value), [selectedGroup]);
+  const filteredProjects = React.useMemo(() => projects.filter(project => (project.authorId === selectedAuth || selectedAuth === 'all') &&
+    (project.group === selectedGroup || selectedGroup === 'all')
+  ),[selectedAuth, selectedGroup]);
+  const filteredAuthors = React.useMemo(() => getFilteredAuthors(filteredProjects), [selectedAuth, selectedGroup]);
   const dateArray = Array(max(filteredProjects.map(project => project.attempt || 0))).fill().map((e, i) => i + 1);
-
+  if (!auth.uid) return <Redirect to='/signin'/>;
   return (
     <div className="card z-depth-0 project-summary summary-container">
       <h3>Admin</h3>
       <p>Выбрать группу: </p>
       <select onChange={onChangeGroup} value={selectedGroup}>{groups.map(group =>
-        <option value={group} selected={group===selectedGroup}>{group}</option>
+        <option value={group} selected={group === selectedGroup}>{group}</option>
       )}</select>
       <p>Выбрать пользователя: </p>
       {/*check that users with same last name have their data under one account*/}
-      <select onChange={onChangeAuth} value={selectedAuth}>{authors.map(author =>
-        <option value={author} selected={author===selectedAuth}>{author}</option>
+      <select onChange={onChangeAuth} value={selectedAuth}>{filteredAuthors.map(author =>
+        <option value={author} selected={author === selectedAuth}>{author}</option>
       )}</select>
 
       <AdminCharts date={dateArray}
-              results={filteredProjects.map(project => project.generalResult ?
-                { result: project.generalResult.shulte, attempt: project.attempt, id: project.authorId } || 0 : 0)}
-              name='Таблицы Шульте'
+                   results={filteredProjects.map(project => project.generalResult ?
+                     {result: project.generalResult.shulte, attempt: project.attempt, id: project.authorId} || 0 : 0)}
+                   name='Таблицы Шульте'
       />
       Снижение показателя говорит об увеличении концентрации внимания
-      {/*<AdminCharts date={dateArray}*/}
-      {/*        results={filteredProjects.map(project => project.generalResult ? project.generalResult.shulteRed || 0 : 0)}*/}
-      {/*        name='Черно-Красные Таблицы Шульте'*/}
-      {/*/>*/}
-      {/*Снижение показателя говорит об улучшении переключаемости внимания*/}
-      {/*<AdminCharts date={dateArray}*/}
-      {/*        results={filteredProjects.map(project => project.generalResult ? project.generalResult.perception || 0 : 0)}*/}
-      {/*        name='Корректурные пробы'*/}
-      {/*/>*/}
-      {/*Снижение показателя говорит об улучшении устойчивости внимания*/}
-      {/*<AdminCharts date={dateArray}*/}
-      {/*        results={filteredProjects.map(project => project.generalResult ? project.generalResult.count || 0 : 0)}*/}
-      {/*        name='Счет'*/}
-      {/*/>*/}
-      {/*Результат близкий к единице говорит о хорошей устойчивости внимания и низкой истощаемости*/}
-      {/*<AdminCharts date={dateArray}*/}
-      {/*        results={filteredProjects.map(project => project.generalResult ? project.generalResult.memoryWords || 0 : 0)}*/}
-      {/*        name='Запоминание слов'*/}
-      {/*/>*/}
-      {/*Увеличение показателя говорит об улучшении памяти*/}
+      <AdminCharts date={dateArray}
+                   results={filteredProjects.map(project => project.generalResult ? {
+                     result: project.generalResult.shulteRed,
+                     attempt: project.attempt,
+                     id: project.authorId
+                   } || 0 : 0)}
+                   name='Черно-Красные Таблицы Шульте'
+      />
+      Снижение показателя говорит об улучшении переключаемости внимания
+      <AdminCharts date={dateArray}
+                   results={filteredProjects.map(project => project.generalResult ? {
+                     result: project.generalResult.perception,
+                     attempt: project.attempt,
+                     id: project.authorId
+                   } || 0 : 0)}
+                   name='Корректурные пробы'
+      />
+      Снижение показателя говорит об улучшении устойчивости внимания
+      <AdminCharts date={dateArray}
+                   results={filteredProjects.map(project => project.generalResult ? {
+                     result: project.generalResult.count,
+                     attempt: project.attempt,
+                     id: project.authorId
+                   } || 0 : 0)}
+                   name='Счет'
+      />
+      Результат близкий к единице говорит о хорошей устойчивости внимания и низкой истощаемости
+      <AdminCharts date={dateArray}
+                   results={filteredProjects.map(project => project.generalResult ? {
+                     result: project.generalResult.memoryWords,
+                     attempt: project.attempt,
+                     id: project.authorId
+                   } || 0 : 0)}
+                   name='Запоминание слов'
+      />
+      Увеличение показателя говорит об улучшении памяти
     </div>
   )
 };
@@ -86,7 +99,6 @@ export const SummaryAdmin = compose(
   connect((state) => ({
     projects: getProjects(state),
     auth: state.firebase.auth,
-    authors: getAuthors(state),
     groups: getGroups(state),
   })),
   firestoreConnect(props => {
